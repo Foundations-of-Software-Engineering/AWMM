f2f9d2f26cc1bf21e975427f1683943475d15876
Cyrus Wang <cyruswang2017@gmail.com>
2024-03-16 20:34:54 -0400

    removing comment

--- src/main/java/com/awmm/messageserver/MessageServerApplication.java
+++ src/main/java/com/awmm/messageserver/MessageServerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@SpringBootApplication(scanBasePackages = {"com.awmm.messageserver.player"})
 public class MessageServerApplication {
 
 	public static void main(String[] args) {
